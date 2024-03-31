57b9e46c1ba35ddb32afaab3c0fba72f956d7f61
emel zorlu <emelzorlu@emel-MacBook-Air.local>
2024-03-31 16:56:50 +0300

    first commit

---
+++ rockpaperscissors/rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by emel zorlu on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
